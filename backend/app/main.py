50052375e3bbc39c21dfed3ff4845615fd53f3eb
Pekka <pekka.levo@hotmail.fi>
2026-02-16 19:40:50 +0200

    Added endpoint /version which informs the version of the project is being used

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from datetime import datetime, timezone
+
+import os 
+import time
 
 app = FastAPI(
     title = "MyProject API",
     version = "0.1.0"
 )
+
+# Simple app start timestamp to calculate uptime
+START_TIME = time.time()
 
 @app.get ("/")
 def root():
@@ -11,4 +18,23 @@ def root():
 
 @app.get("/health")
 def health():
-    return {"status": "ok"}
+    """
+    Health check endpoint for monitoring.
+    """
+    return {
+        "status": "ok",
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "uptime_seconds": int(time.time() - START_TIME),
+    }
+
+@app.get("/version")
+def version():
+    """
+    Version endpoint
+    if GIT_SHA is provided as an enviroment variable, it will be included.
+    """
+    return{
+        "name": app.title,
+        "version": app.version,
+        "git-sha": os.getenv("GIT_SHA"),
+    }
